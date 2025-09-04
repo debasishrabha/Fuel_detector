412c6fbf240913ac76c3b5d3a956048f1dee0cc1
debasish69 <debasishrva78@gmail.com>
2025-09-04 16:29:56 +0530

    update

--- app.py
+++ app.py
@@ -646,4 +646,5 @@ def reset_password(token):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
